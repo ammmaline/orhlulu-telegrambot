eaacd374e01708695b510b2d2d465edd1d902615
Amaline Lim <105907243+ammmaline@users.noreply.github.com>
2022-09-25 20:46:10 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+
+const TOKEN = process.env.TOKEN;
+
+const bot = new TelegramBot(TOKEN, {polling: true});
+
+bot.on('message', (message) => {
+
+        let chat_id = message.from.id;
+        //console.log(message)
+        //console.log(message.from.id);
+        //middleware 
+        bot.sendMessage(chat_id, 'Hello from orhLuLu');
+
+});
